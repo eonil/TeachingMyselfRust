9e231229fc04455005c8634003076a5870431d69
Hoon H <drawtree@me.com>
2014-08-20 00:07:12 +0900

    Working...

--- a3/run.bash
+++ a3/run.bash
@@ -1,7 +1,7 @@
 rm -rf ./Build
 mkdir ./Build
 
-rustc a.rs --crate-type=staticlib -o ./Build/rust-stuffs.a
+rustc -g a.rs --crate-type=staticlib -o ./Build/rust-stuffs.a
 clang b.c ./Build/rust-stuffs.a -o Build/out
 
 cd Build
